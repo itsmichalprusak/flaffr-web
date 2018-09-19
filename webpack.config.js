aa08b56cc917297ee63ab47cdbfe403d5641bd64
Michał Prusak <meehoweq@gmail.com>
2018-09-19 23:55:56 +0200

    Deleted 'resolve' part of the webpack config so that it resolves correctly. Tricky, huh?

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,6 @@ const config = {
     path: path.resolve(__dirname, 'build'),
     filename: 'flaffr-bundled.js'
   },
-  resolve: {
-    extensions: ['.ts', '.tsx', 'js']
-  },
   module: {
     rules: [
       { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }
