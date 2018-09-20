e48fb19ce9e394005d7fa24be022f2d2eb88961f
Michał Prusak <meehoweq@gmail.com>
2018-09-20 19:41:15 +0200

    Beginning to implement webpack caching and optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 const config = {
   entry: ['./src/App.tsx'],
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Flaffr.com' // The title that will be included in outputted HTML file.
+    })
+  ],
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'flaffr-bundled.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-[contenthash].bundle.js'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'] // https://github.com/ahfarmer/minimal-react-starter/issues/4
@@ -13,6 +22,18 @@ const config = {
       { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }
       // TODO: Write rules and general support for SCSS
     ]
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
   }
 }
 
