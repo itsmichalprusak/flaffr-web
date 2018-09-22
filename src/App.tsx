192bad4c3d52ef1ef1289491229a9ccdc9b1f695
Michał Prusak <meehoweq@gmail.com>
2018-09-22 21:21:37 +0200

    Changed the basic stuff into a dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import Dashboard from "./components/Dashboard";
 
-/**
+/*
  * Get the React DOM and init our app.
- * TODO: Change this to actual app component
  */
 ReactDOM.render(
-    <div>
-        <h1>Hello world</h1>
-    </div>,
+    <Dashboard/>,
     document.getElementById('root') // The element we are mounting the virtual DOM on
 )
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+/**
+ * The main app container.
+ */
+export default class Dashboard extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hallo Welt</h1>
+            </div>
+        )
+    }
+}
